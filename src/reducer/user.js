73e4f01dbdb94e034c8f9f653381eb08119d83b5
Michael Stuart <mike.w.stuart@gmail.com>
2017-08-15 10:40:49 -0700

    connecting auth actions/reducers/routes

--- src/reducer/user.js
+++ src/reducer/user.js
@@ -1,16 +1,13 @@
-const validateUser = (payload) => {
-  if (!payload._id) throw new Error('VALIDATION ERROR: user must have _id');
-  if (!payload.name) throw new Error('VALIDATION ERROR: user must have name');
-};
-
 export default (state = [], action) => {
   const { type, payload } = action;
   switch (type) {
-    case 'USER_SIGNUP':
-      validateUser(payload);
-      return payload;
     case 'USER_LOGIN':
-      validateUser(payload);
+      console.log(payload)
+      return payload;
+    case 'USER_LOGOUT':
+      () => {
+        console.log(payload)
+      }
       return payload;
     default:
       return state;
